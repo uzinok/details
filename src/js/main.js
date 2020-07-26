29319e34f44d7d0d86c63abb27eaa5a5dbb170a5
uzinok <uzinok@yandex.ru>
2020-07-26 14:17:04 +0400

    при открытом в html коде details не сразу работает спойлер

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,59 @@
 window.onload = function () {
+    // ролучаем все спойлеры
     let arrDetails = document.querySelectorAll('details');
 
+    // перебираем все спойлееры для получения необходимых элементов и прослушивания событий
     for (let i = 0; i < arrDetails.length; i++) {
-        arrDetails[i].addEventListener('click', function (event) {
-            if (this.classList.contains('details-open')) {
+
+        // по кнопке summary будем отслеживать клик
+        let elemSummary;
+        // если summary отсутствует - добавляем
+        if (!(arrDetails[i].querySelector('summary'))) {
+            elemSummary = document.createElement('summary');
+            elemSummary.innerHTML = 'подробнее';
+
+            arrDetails[i].insertBefore(elemSummary, arrDetails[i].querySelector('.details_hide'))
+        }
+        // если summary есть - получаем 
+        else {
+            elemSummary = arrDetails[i].querySelector('summary');
+        }
+
+        // для старых браузеров скрываем контент что бы не было доступа к интерактивным элементам
+        arrDetails[i].classList.add('details_h');
+
+        // отслеживаем клик
+        elemSummary.addEventListener('click', function (event) {
+            
+            // получаем родительский блок
+            let parentBlock = this.parentNode;
+
+            // если открыт спойлер
+            if (parentBlock.classList.contains('details-visible')) {
+                // отменяем действие по умолчанию для браузеров поддерживоющих <details>
                 event.preventDefault();
-                this.classList.remove('details-open');
+                // с анимацией скрываем контент
+                parentBlock.classList.remove('details-visible');
+
+                // после завершения анимации
                 setTimeout(function () {
-                    arrDetails[i].open = false;
+                    // для старых браузеров скрываем контент что бы не было доступа к интерактивным элементам
+                    parentBlock.classList.add('details_h');
+                    // для новых - удаляем атрибут open
+                    parentBlock.open = false;
                 }, 200)
-            } else {
+            }
+            // если спойлер закрыт 
+            else {
+                // для старых браузеров удаляем display none для контента
+                parentBlock.classList.remove('details_h');
+                // через минимальный промежуток времени запускаем анимацию css появления контента
                 setTimeout(function () {
-                    console.log(i)
-                    arrDetails[i].classList.add('details-open')
+                    parentBlock.classList.add('details-visible');
                 }, 5)
             }
         })
+
+
     }
 };
