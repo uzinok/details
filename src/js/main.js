cdbb79ee8520dd3ead842580f4bb49a2290b4e5e
uzinok <uzinok@yandex.ru>
2020-07-27 13:19:52 +0400

    для старых браузеров необходимо заменить summary на button

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,9 @@
 window.onload = function () {
+    // добавим стили для анимации
+    let styleD = document.createElement('style');
+    styleD.innerHTML = '.details_hide{overflow:hidden;opacity:0;max-height:0;transition:max-height 200ms,opacity 200ms}.details-visible .details_hide{overflow:auto;opacity:1;max-height:190px}.details_h .details_hide{display:none}';
+    // добавим стили в head
+    document.head.appendChild(styleD);
     // ролучаем все спойлеры
     let arrDetails = document.querySelectorAll('details');
 
@@ -9,22 +14,40 @@ window.onload = function () {
         let elemSummary;
         // если summary отсутствует - добавляем
         if (!(arrDetails[i].querySelector('summary'))) {
-            elemSummary = document.createElement('summary');
+            // если нет поддержки details
+            if (!('open' in arrDetails[i]))
+                // создаем кнопку
+                elemSummary = document.createElement('button');
+            else
+                // или содаем summary
+                elemSummary = document.createElement('summary');
+                // добавим текст по умолчанию
             elemSummary.innerHTML = 'подробнее';
-
+            // добавляем текущему спойлеру
             arrDetails[i].insertBefore(elemSummary, arrDetails[i].querySelector('.details_hide'))
         }
         // если summary есть - получаем 
         else {
             elemSummary = arrDetails[i].querySelector('summary');
+            // если нет поддержки details 
+            if (!('open' in arrDetails[i]))
+            // тут необходимо вместо summary сделать button 
+                elemSummary.setAttribute('tabindex', 1)
         }
 
+        // если спойлер открыт
+        if (arrDetails[i].hasAttribute('open')) {
+            // добавляем класс спойлеру что бы контент был виден
+            arrDetails[i].classList.add('details-visible');
+        }
         // для старых браузеров скрываем контент что бы не было доступа к интерактивным элементам
-        arrDetails[i].classList.add('details_h');
+        else if (!('open' in arrDetails[i])) {
+            arrDetails[i].classList.add('details_h');
+        }
 
         // отслеживаем клик
         elemSummary.addEventListener('click', function (event) {
-            
+
             // получаем родительский блок
             let parentBlock = this.parentNode;
 
@@ -37,10 +60,12 @@ window.onload = function () {
 
                 // после завершения анимации
                 setTimeout(function () {
-                    // для старых браузеров скрываем контент что бы не было доступа к интерактивным элементам
-                    parentBlock.classList.add('details_h');
-                    // для новых - удаляем атрибут open
-                    parentBlock.open = false;
+                    if (!('open' in parentBlock))
+                        // для старых браузеров скрываем контент что бы не было доступа к интерактивным элементам
+                        parentBlock.classList.add('details_h');
+                    else
+                        // для новых - удаляем атрибут open
+                        parentBlock.open = false;
                 }, 200)
             }
             // если спойлер закрыт 
@@ -53,7 +78,5 @@ window.onload = function () {
                 }, 5)
             }
         })
-
-
     }
 };
